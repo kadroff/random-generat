2be802bc0e7e62cd683d2592a3a56fe1249a81e7
Marsel Kadrov <marselkadroff@gmail.com>
2016-03-03 23:32:53 +0500

    включил выбор диапазона чисел

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
   $("button[name=generat]").click(function(){
     function getRandomArbitrary(min, max) {
-      var min = 0;
-      var max = 10;
-      return Math.random() * (max - min) + min;
+      var value = $("select[name=value]").val();
+      var max;
+      var min = 1;
 
+      if (value == "10"){
+        max = 10;
+    }else if (value == "50"){
+        max = 50;
+    }else if (value == "100"){
+        max = 100;
+      };
+
+      Math.random() * (max + min) - min;
 };
     $("input[name=result]").val(getRandomArbitrary);
   });
-
-
 });
